ec1dd24c167148c50713ad1bef5340c2a65f5b7e
Pavle <pavlemilovanovic42@gmail.com>
2025-07-12 23:30:09 +0200

    Finish the exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, timesToRepeat) {
+    if (timesToRepeat < 0) return 'ERROR';
 
+    let returnString = '';
+    for (let i = 0; i < timesToRepeat; i++) {
+        returnString += word;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
