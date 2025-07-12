b3607e7b6bf204d5b44e65bd354975810ba03049
Pavle <pavlemilovanovic42@gmail.com>
2025-07-12 23:43:09 +0200

    Finish the exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = [];
+    for (let i = 0; i < word.length / 2; i++) {
+        reversedWord[i] = word[word.length - 1 - i];
+        reversedWord[word.length - 1 - i] = word[i];
+    }
 
+    return reversedWord.join('');
 };
+
+// I could've done this as well: return word.split('').reverse().join('')
 
 // Do not edit below this line
 module.exports = reverseString;
