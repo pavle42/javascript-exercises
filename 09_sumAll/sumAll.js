873d17e70695b1334d18841e72db87c3c72e5279
Pavle <pavlemilovanovic42@gmail.com>
2025-07-13 00:05:11 +0200

    Finish the exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0) return 'ERROR';
+    if (first % 1 !== 0 || second % 1 !== 0) return 'ERROR';
+    if (typeof first !== 'number' || typeof second !== 'number') return 'ERROR';
 
+    if (first > second) {
+        [first, second] = [second, first]
+    }
+    let sum = 0;
+    for (let i = first; i <= second; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
