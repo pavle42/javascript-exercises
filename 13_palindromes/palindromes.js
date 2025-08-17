d1c89d67e48dfc449ae66c4cc17d149889268b80
Pavle <pavlemilovanovic42@gmail.com>
2025-08-17 13:56:31 +0200

    Finish the exercise 13_PALINDROMES

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.toLowerCase();
 
+    let leftPointer = 0;
+    let rightPointer = text.length;
+
+    while (leftPointer < rightPointer) {
+        while (!isAlphanumeric(text[leftPointer])) {
+            leftPointer++;
+        }
+
+        while (!isAlphanumeric(text[rightPointer])) {
+            rightPointer--;
+        }
+
+        if (text[leftPointer] != text[rightPointer]) {
+            return false;
+        } else {
+            leftPointer++;
+            rightPointer--;
+        }
+    }
+
+    return true;
 };
+
+function isAlphanumeric(ch) {
+    return /^[A-Za-z0-9]$/.test(ch);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
