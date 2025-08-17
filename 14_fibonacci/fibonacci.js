2fe73d7a2a29e33607619fcaac78419cd22a028a
Pavle <pavlemilovanovic42@gmail.com>
2025-08-17 14:31:33 +0200

    Finish the exercise 14_FIBONACCI

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) return 0;
+    if (num < 0) return "OOPS";
 
+    let lastTwo = [1, 1];
+    let current = 1;
+
+    for (let i = 2; i < num; i++) {
+        current = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
