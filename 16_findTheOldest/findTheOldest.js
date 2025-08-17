8eb5e6966d2a6ea00fec2c54e74849e534c3d112
Pavle <pavlemilovanovic42@gmail.com>
2025-08-17 18:08:23 +0200

    Finish the exercise 16_FINDTHEOLDEST

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, cur) => {
+        if (!("yearOfDeath" in cur)) {
+            cur.yearOfDeath = new Date().getFullYear();
+        }
 
+        if (acc.yearOfDeath - acc.yearOfBirth < cur.yearOfDeath - cur.yearOfBirth) {
+            acc = cur;
+        }
+        
+        return acc;
+    }, arr[0])
 };
 
 // Do not edit below this line
